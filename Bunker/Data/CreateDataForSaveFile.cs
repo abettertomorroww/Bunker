0edd05452df2540380c9f0ea7bf5bff9b1821c4b
LoginovAV <43448403+abettertomorroww@users.noreply.github.com>
2022-05-30 16:38:22 +0400

    Update CreateDataForSaveFile.cs

--- Bunker/Data/CreateDataForSaveFile.cs
+++ Bunker/Data/CreateDataForSaveFile.cs
@@ -48,32 +48,32 @@ namespace Bunker.Data
 
         private string[][] FormingRandomProperties(int playersCount, string disaster, Random rnd, string animals)
         {
-            string[] propertyPlayer = new string[9];
+            string[] propertiesPlayer = new string[9];
             string[][] allPlayers = new string[playersCount][];
             int oldNumber = 0;
             int newNumber = 0;
 
             for (int i = 0; i < playersCount; i++)
             {
-                propertyPlayer[0] = specifications.Profession[rnd.Next(0, specifications.Profession.Count)];
-                propertyPlayer[1] = specifications.Health[rnd.Next(0, specifications.Health.Count)];
-                propertyPlayer[2] = specifications.Personality[rnd.Next(0, specifications.Personality.Count)];
-                propertyPlayer[3] = specifications.Hobby[rnd.Next(0, specifications.Hobby.Count)];
-                propertyPlayer[4] = specifications.Phobia[rnd.Next(0, specifications.Phobia.Count)];
-                propertyPlayer[5] = specifications.Add_Info[rnd.Next(0, specifications.Add_Info.Count)];
-                propertyPlayer[6] = specifications.Luggage[rnd.Next(0, specifications.Luggage.Count)];
+                propertiesPlayer[0] = specifications.Profession[rnd.Next(0, specifications.Profession.Count)];
+                propertiesPlayer[1] = specifications.Health[rnd.Next(0, specifications.Health.Count)];
+                propertiesPlayer[2] = specifications.Personality[rnd.Next(0, specifications.Personality.Count)];
+                propertiesPlayer[3] = specifications.Hobby[rnd.Next(0, specifications.Hobby.Count)];
+                propertiesPlayer[4] = specifications.Phobia[rnd.Next(0, specifications.Phobia.Count)];
+                propertiesPlayer[5] = specifications.Add_Info[rnd.Next(0, specifications.Add_Info.Count)];
+                propertiesPlayer[6] = specifications.Luggage[rnd.Next(0, specifications.Luggage.Count)];
 
                 oldNumber = rnd.Next(0, specifications.Card.Count);
-                propertyPlayer[7] = specifications.Card[oldNumber];
+                propertiesPlayer[7] = specifications.Card[oldNumber];
                 newNumber = rnd.Next(0, specifications.Card.Count);
                 if (newNumber == oldNumber)
                 {
                     newNumber = rnd.Next(0, specifications.Card.Count);
-                    propertyPlayer[8] = specifications.Card[newNumber];
+                    propertiesPlayer[8] = specifications.Card[newNumber];
                 }
-                else propertyPlayer[8] = specifications.Card[newNumber];
+                else propertiesPlayer[8] = specifications.Card[newNumber];
 
-                allPlayers[i] = FormingFinalProperties(propertyPlayer, disaster, rnd, animals);
+                allPlayers[i] = FormingFinalProperties(propertiesPlayer, disaster, rnd, animals);
             }
             return allPlayers;
         }
@@ -86,32 +86,32 @@ namespace Bunker.Data
             int weight = rnd.Next(38, 140);
             double IIB = weight / Math.Pow(height*0.01, 2);
             
-            string[] propertyPlayer = new string[15];
-            propertyPlayer[0] = "Катастрофа: " + disaster;
-            propertyPlayer[1] = animals;
-            propertyPlayer[2] = "Профессия: " + playerProp[0];
-            propertyPlayer[3] = "Возраст: " + rnd.Next(16, 96);
-            if (child == 1) propertyPlayer[4] = "Деторождение: Childfree";
-            else propertyPlayer[4] = "Деторождение: не Childfree";
-            if (age == 1) propertyPlayer[5] = "Пол: Мужчина";
-            else propertyPlayer[5] = "Пол: Женщина";
+            string[] propertiesPlayer = new string[15];
+            propertiesPlayer[0] = "Катастрофа: " + disaster;
+            propertiesPlayer[1] = animals;
+            propertiesPlayer[2] = "Профессия: " + playerProp[0];
+            propertiesPlayer[3] = "Возраст: " + rnd.Next(16, 96);
+            if (child == 1) propertiesPlayer[4] = "Деторождение: Childfree";
+            else propertiesPlayer[4] = "Деторождение: не Childfree";
+            if (age == 1) propertiesPlayer[5] = "Пол: Мужчина";
+            else propertiesPlayer[5] = "Пол: Женщина";
 
-            if (IIB < 18.5) propertyPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Вес ниже нормы";
-            else if (IIB >= 18.5 && IIB < 25) propertyPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Норма";
-            else if (IIB >= 25 && IIB < 30) propertyPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Избыточный вес";
-            else if (IIB >= 30 && IIB < 35) propertyPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Ожирение I степени";
-            else if (IIB >= 35 && IIB < 40) propertyPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Ожирение II степени";
-            else if (IIB >= 40) propertyPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Ожирение III степени";
+            if (IIB < 18.5) propertiesPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Вес ниже нормы";
+            else if (IIB >= 18.5 && IIB < 25) propertiesPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Норма";
+            else if (IIB >= 25 && IIB < 30) propertiesPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Избыточный вес";
+            else if (IIB >= 30 && IIB < 35) propertiesPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Ожирение I степени";
+            else if (IIB >= 35 && IIB < 40) propertiesPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Ожирение II степени";
+            else if (IIB >= 40) propertiesPlayer[6] = $"Рост: {height} см. Вес: {weight}кг. ИМТ: Ожирение III степени";
 
-            propertyPlayer[7] = "Здоровье: " + playerProp[1];
-            propertyPlayer[8] = "Черта характера: " + playerProp[2];
-            propertyPlayer[9] = "Фоббия: " + playerProp[4];
-            propertyPlayer[10] = "Хобби: " + playerProp[3];
-            propertyPlayer[11] = "Доп. информация: " + playerProp[5];
-            propertyPlayer[12] = "Багаж: " + playerProp[6];
-            propertyPlayer[13] = "Карта 1: " + playerProp[7];
-            propertyPlayer[14] = "Карта 2: " + playerProp[8];
-            return propertyPlayer;
+            propertiesPlayer[7] = "Здоровье: " + playerProp[1];
+            propertiesPlayer[8] = "Черта характера: " + playerProp[2];
+            propertiesPlayer[9] = "Фоббия: " + playerProp[4];
+            propertiesPlayer[10] = "Хобби: " + playerProp[3];
+            propertiesPlayer[11] = "Доп. информация: " + playerProp[5];
+            propertiesPlayer[12] = "Багаж: " + playerProp[6];
+            propertiesPlayer[13] = "Карта 1: " + playerProp[7];
+            propertiesPlayer[14] = "Карта 2: " + playerProp[8];
+            return propertiesPlayer;
         }
     }
 }
